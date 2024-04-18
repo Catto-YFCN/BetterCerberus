633b81a363c9ed0d8e44e7c11bb1d9e187e7e855
Catto <66782332+Catto-YFCN@users.noreply.github.com>
2024-04-18 17:29:07 +0700

    Update Cerberus Docs.lua

--- Cerberus Docs.lua
+++ Cerberus Docs.lua
@@ -1,4 +1,4 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Jxereas/UI-Libraries/main/cerberus.lua"))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Catto-YFCN/BetterCerberus/main/Cerberus.lua"))()
 
 local window = Library.new("Window") -- Args(<string> Name, <boolean?> ConstrainToScreen)
 
